23f86b575f996f075030b5a421807f406905197c
anaelj <anaelj@gmail.com>
2021-08-15 15:34:03 -0400

    add cors config

--- server.js
+++ server.js
@@ -7,14 +7,28 @@ import { getPricesMagazine } from "./src/scrap/magazine.js";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ['GET']
+  })
+);
 const port = process.env.PORT || "3333";
 app.set("port", port);
 
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
+
 app.use("/prices", async (req, res) => {
   const pricesKabum = await getPricesKabum(req.query.textSearch);
-  const pricesPichau = await getPricePichau(req.query.textSearch)
-  const pricesMagazine = await getPricesMagazine(req.query.textSearch)
+  const pricesPichau = await getPricePichau(req.query.textSearch);
+  const pricesMagazine = await getPricesMagazine(req.query.textSearch);
 
   const prices = [...pricesKabum.concat(pricesPichau).concat(pricesMagazine)];
   // console.log(req.query.textSearch)
