c6dee74448e154227d7b362de9125d802f5c5779
anaelj <anaelj@gmail.com>
2021-08-15 11:07:27 -0400

    add test route

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 import express from "express";
 import router from "./src/routes/index.js";
 import cors from "cors";
-import {getPricesKabum} from "./src/scrap/kabum.js"
-
+import { getPricesKabum } from "./src/scrap/kabum.js";
 
 const app = express();
 app.use(express.json());
@@ -10,16 +9,19 @@ app.use(cors());
 const port = process.env.PORT || "3333";
 app.set("port", port);
 
-app.use("/prices", async (req, res)=> {
-
-    const prices = await getPricesKabum("rtx 2060")
-    res.send(prices);
-
+app.use("/prices", async (req, res) => {
+  const prices = await getPricesKabum("rtx 2060");
+  res.send(prices);
 });
+
+app.use("/teste", (req, res) => {
+  res.send({ name: "teste" });
+});
+
 app.listen(port, async () => {
-    try {
-        console.log ("Api Started");
-    } catch (error) {
-        console.log('error')
-    }
-})
+  try {
+    console.log("Api Started");
+  } catch (error) {
+    console.log("error");
+  }
+});
