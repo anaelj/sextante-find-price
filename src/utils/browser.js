282822a858fa695488a2ccc1314bf9fc011a46d5
anaelj <anaelj@gmail.com>
2021-09-02 14:10:39 -0400

    sandbox

--- src/utils/browser.js
+++ src/utils/browser.js
@@ -3,7 +3,6 @@ import puppeteer from "puppeteer";
 export const getBrownser = async (showBrowser) => {
 
 //  return  await puppeteer.launch({ headless: !showBrowser });
-    
  return browser = await puppeteer.launch({
         'args' : [
           '--no-sandbox',
