4c33567f1b3cf331ddb72596ffeed10a5c004911
anaelj <anaelj@gmail.com>
2021-09-02 14:03:18 -0400

    sandbox

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import express from "express";
-import router from "./src/routes/index.js";
+// import router from "./src/routes/index.js";
 import cors from "cors";
 import { getPricesKabum } from "./src/scrap/kabum.js";
 import { getPricePichau } from "./src/scrap/pichau.js";
 import { getPricesMagazine } from "./src/scrap/magazine.js";
-import { getSubmarino } from "./src/scrap/submarino.js";
+import { getPriceSubmarino } from "./src/scrap/submarino.js";
 import { getPricesCasasbahia } from "./src/scrap/casasbahia.js";
-
+import { getPagesAmazon } from "./src/scrap/amazon.js";
 
 const app = express();
 app.use(express.json());
@@ -27,23 +27,35 @@ app.use((req, res, next) => {
 });
 
 app.use("/prices", async (req, res) => {
-
-  const prices = []; // await getPricesKabum(req.query.textSearch); 
+  const prices = []; // await getPricesKabum(req.query.textSearch);
 
   console.log(
     "---------------------------------------- start ------------------------------"
   );
+
+  // var p1 = new Promise((resolve, reject) => {
+  //   setTimeout(() => resolve([]), 10000);
+  // });
   await Promise.all([
     getPricesKabum(req.query.textSearch, true),
     getPricePichau(req.query.textSearch, true),
     getPricesMagazine(req.query.textSearch, true),
-    getSubmarino(req.query.textSearch, true),
-    getPricesCasasbahia(req.query.textSearch, true)
+    getPriceSubmarino(req.query.textSearch, true),
+    getPricesCasasbahia(req.query.textSearch, true),
+    getPagesAmazon(req.query.textSearch, true)
   ]).then((values) => {
-    prices.push(...values[0].concat(values[1]).concat(values[2]).concat(values[3]).concat(values[4]));
+    prices.push(
+      // ...values[0]
+      ...values[0]
+        .concat(values[1])
+        .concat(values[2])
+        .concat(values[3])
+        .concat(values[4])
+        .concat(...values[5])
+    );
     //prices.push(values);
-
   });
+  // console.log(prices);
   console.log(
     "---------------------------------------- end thread ------------------------------"
   );
---
+++ src/utils/browser.js
@@ -0,0 +1,13 @@
+import puppeteer from "puppeteer";
+
+export const getBrownser = async (showBrowser) => {
+
+//  return  await puppeteer.launch({ headless: !showBrowser });
+    
+ return browser = await puppeteer.launch({
+        'args' : [
+          '--no-sandbox',
+          '--disable-setuid-sandbox'
+        ]
+      });
+}
